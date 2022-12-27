054271489285c5ee69ad1d515d0ceea84622ea4c
Ticap68 <121486492+Ticap68@users.noreply.github.com>
2022-12-27 20:58:46 +0100

    doc google

--- fonctions.js
+++ fonctions.js
@@ -15,12 +15,29 @@ function getAltitudeGpxz() {
     .then(data => {
         console.log(data.results[0].elevation);
         document.getElementById('altitude').innerHTML = data.results[0].elevation;
-    })
+    });
     //.catch(error => alert("Erreur : " + error));
  }
 
+ var axios = require('axios');
 
- window.onload = getAltitude;
+ var config = {
+   method: 'get',
+   url: 'https://maps.googleapis.com/maps/api/elevation/json?locations=39.7391536%2C-104.9847034&key=YOUR_API_KEY',
+   headers: { }
+ };
+ 
+ axios(config)
+ .then(function (response) {
+   console.log(JSON.stringify(response.data));
+ })
+ .catch(function (error) {
+   console.log(error);
+ });
+
+
+
+ //window.onload = getAltitude;
 
 
   
