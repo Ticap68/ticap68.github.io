5d4ac25d3a2decb63d84dd36d48880be8366af77
Ticap68 <121486492+Ticap68@users.noreply.github.com>
2022-12-27 14:29:05 +0100

    Update fonctions.js

--- fonctions.js
+++ fonctions.js
@@ -12,7 +12,8 @@ function getAltitudeGpxz() {
     fetch('https://maps.googleapis.com/maps/api/elevation/json?locations=48.856614,2.3522219&key=AIzaSyA8A69egVsPgDDrAXVWXdtW_HrpKLvmiOM', {mode: 'no-cors'})
     .then(response => response.json())
     .then(data => {
-    console.log(data.results[0].elevation);
+        console.log(data.results[0].elevation);
+        document.getElementById('altitude').innerHTML = data.elevation;
     });
  }
 
