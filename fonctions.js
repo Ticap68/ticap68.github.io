0edbf7e85be16240654917a79b84df1b09c7e3e7
Ticap68 <121486492+Ticap68@users.noreply.github.com>
2022-12-27 12:57:27 +0100

    Create fonctions.js

---
+++ fonctions.js
@@ -0,0 +1,24 @@
+function getAltitudeGpxz() {
+    fetch('https://gpxz.com/api/elevation?lat=48.856614&lon=2.3522219000000177')
+      .then(response => response.json())
+      .then(data => {
+        document.getElementById('altitude').innerHTML = data.elevation;
+      });
+  }
+  
+
+
+ function getAltitude() {
+    
+    fetch('https://maps.googleapis.com/maps/api/elevation/json?locations=48.856614,2.3522219&key=AIzaSyA8A69egVsPgDDrAXVWXdtW_HrpKLvmiOM')
+    .then(response => response.json())
+    .then(data => {
+    console.log(data.results[0].elevation);
+    });
+ }
+
+
+ window.onload = getAltitude;
+
+
+  
