1c1d1357333f76f0eabbaa064816616a11c74a99
Ticap68 <121486492+Ticap68@users.noreply.github.com>
2022-12-28 10:27:10 +0100

    forme

--- fonctions.js
+++ fonctions.js
@@ -1,33 +1,35 @@
 function getAltitudeGpxz() {
-    fetch('https://gpxz.com/api/elevation?lat=48.856614&lon=2.3522219000000177')
-      .then(response => response.json())
-      .then(data => {
-        document.getElementById('altitude').innerHTML = data.elevation;
-      });
-  }
-  
+  fetch('https://gpxz.com/api/elevation?lat=48.856614&lon=2.3522219000000177')
+    .then(response => response.json())
+    .then(data => {
+      document.getElementById('altitude').innerHTML = data.elevation;
+    });
+}
 
+function getAltitude() {
+  fetch('https://maps.googleapis.com/maps/api/elevation/json?locations=48.856614,2.3522219&key=AIzaSyA8A69egVsPgDDrAXVWXdtW_HrpKLvmiOM', { mode: 'cors' })
+    .then(response => response.json())
+    //.catch(error=>console.log(error))
+    .then(data => {
+      console.log(data.results[0].elevation);
+    });
+}
 
- function getAltitude(){fetch('https://maps.googleapis.com/maps/api/elevation/json?locations=48.856614,2.3522219&key=AIzaSyA8A69egVsPgDDrAXVWXdtW_HrpKLvmiOM',{mode:'cors'}).then(response=>response.json()).catch(error=>console.log(error)).then(data =>{console.log(data.results[0].elevation);document.getElementById('altitude').innerHTML=data.results[0].elevation;});
-   
-                        
-  
-                        
- //.catch(error => alert("Erreur : " +error));}
+//.catch(error => alert("Erreur : " +error));}
+//document.getElementById('altitude').innerHTML = data.results[0].elevation;
 
- var axios = require('axios');
+var axios = require('axios');
+var config = {
+  method: 'get',
+  url: 'https://maps.googleapis.com/maps/api/elevation/json?locations=39.7391536%2C-104.9847034&key=AIzaSyA8A69egVsPgDDrAXVWXdtW_HrpKLvmiOM',
+  headers: {}
+};
 
- var config = {
-   method: 'get',
-   url: 'https://maps.googleapis.com/maps/api/elevation/json?locations=39.7391536%2C-104.9847034&key=AIzaSyA8A69egVsPgDDrAXVWXdtW_HrpKLvmiOM',
-   headers: { }
- };
- 
- axios(config).then(function(response){console.log(JSON.stringify(response.data));}).catch(function(error){console.log(error);});
+axios(config).then(function (response) { console.log(JSON.stringify(response.data)); }).catch(function (error) { console.log(error); });
 
 
 
  //window.onload = getAltitude;
 
 
-  
+
