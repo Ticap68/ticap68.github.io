075529babdb4549226f003d503aa563ee443bb84
Ticap68 <121486492+Ticap68@users.noreply.github.com>
2022-12-28 11:46:34 +0100

    A

--- fonctions.js
+++ fonctions.js
@@ -31,7 +31,7 @@ axios(config)
   .then(function (response) {
     console.log(JSON.stringify(response.data));
     console.log(response.data.results[0].elevation);
-    document.getElementById('altitude').innerHTML = data.elevation;
+    document.getElementById('altitude').innerHTML = response.data.results[0].elevation;
   })
   .catch(function (error) {
     console.log(error);
