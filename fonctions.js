4b692f49bc294f832bd4c6ce60ba814b6ab2179b
Ticap68 <121486492+Ticap68@users.noreply.github.com>
2022-12-28 09:45:37 +0100

    Update fonctions.js

--- fonctions.js
+++ fonctions.js
@@ -8,16 +8,12 @@ function getAltitudeGpxz() {
   
 
 
- function getAltitude() {
-    fetch('https://maps.googleapis.com/maps/api/elevation/json?locations=48.856614,2.3522219&key=AIzaSyA8A69egVsPgDDrAXVWXdtW_HrpKLvmiOM', {mode: 'cors'})
-    .then(response => response.json())
-    .catch(error => console.log(error))
-    .then(data => {
-        console.log(data.results[0].elevation);
-        document.getElementById('altitude').innerHTML = data.results[0].elevation;
-    });
-    //.catch(error => alert("Erreur : " + error));
- }
+ function getAltitude(){fetch('https://maps.googleapis.com/maps/api/elevation/json?locations=48.856614,2.3522219&key=AIzaSyA8A69egVsPgDDrAXVWXdtW_HrpKLvmiOM',{mode:'cors'}).then(response=>response.json()).catch(error=>console.log(error)).then(data =>{console.log(data.results[0].elevation);document.getElementById('altitude').innerHTML=data.results[0].elevation;});
+   
+                        
+  
+                        
+ //.catch(error => alert("Erreur : " +error));}
 
  var axios = require('axios');
 
@@ -27,13 +23,7 @@ function getAltitudeGpxz() {
    headers: { }
  };
  
- axios(config)
- .then(function (response) {
-   console.log(JSON.stringify(response.data));
- })
- .catch(function (error) {
-   console.log(error);
- });
+ axios(config).then(function(response){console.log(JSON.stringify(response.data));}).catch(function(error){console.log(error);});
 
 
 
