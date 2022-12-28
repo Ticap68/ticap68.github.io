59b2558dd082cc806bfbcd927cee2252bafbdace
Ticap68 <121486492+Ticap68@users.noreply.github.com>
2022-12-28 11:35:56 +0100

    ty

--- fonctions.js
+++ fonctions.js
@@ -24,12 +24,13 @@ function getAltitude() {
 var config = {
   method: 'get',
   url: 'https://api.open-elevation.com/api/v1/lookup?locations=41.161758,-8.583933',
-  headers: {}
+  headers: {'interest-cohort'}
 };
 
 axios(config)
   .then(function (response) {
     console.log(JSON.stringify(response.data));
+    console.log(response.data.results[0].elevation);
   })
   .catch(function (error) {
     console.log(error);
