988113e68eb10d05351e02249c5c82dd29545d40
Ticap68 <121486492+Ticap68@users.noreply.github.com>
2022-12-29 14:21:44 +0000

    h

--- fonctions.js
+++ fonctions.js
@@ -1,7 +1,7 @@
-const map = L.map('map').setView([48.043, 7.156], 13);
+const map = L.map('map').setView([48.043, 7.156], 10);
 
 const tiles = L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
-  maxZoom: 19,
+  maxZoom: 25,
   attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
 }).addTo(map);
 
