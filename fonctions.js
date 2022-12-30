4a55af9bbc197491215327192fc33c3c3d4f6c42
Ticap68 <121486492+Ticap68@users.noreply.github.com>
2022-12-30 11:49:46 +0000

    gf

--- fonctions.js
+++ fonctions.js
@@ -15,7 +15,7 @@ var marker = new L.Marker([]);
 map.on('click', function (e) {
 
 
-  if (markerA = null) { markerA = L.marker(e.latlng).addTo(map); }
+  if (markerA == null) { markerA = L.marker(e.latlng).addTo(map); }
 
   document.getElementById('latlng').innerHTML = markerA;
 
