e98be4f0d6e297631179950cc41e56972dc99c64
Ticap68 <121486492+Ticap68@users.noreply.github.com>
2023-08-20 15:46:05 +0200

    v

--- ajouter.php
+++ ajouter.php
@@ -1,6 +1,6 @@
 <?php
 // Lire le contenu du fichier JSON
-$jsonData = file_get_contents('sellettes.json');
+$jsonData = file_get_contents('liste_sellettes.json');
 $data = json_decode($jsonData, true);
 
 // Récupérer les données du formulaire
@@ -19,7 +19,7 @@ $newParaglider = array(
 $data[] = $newParaglider;
 
 // Écrire le tableau mis à jour dans le fichier JSON
-file_put_contents('sellettes.json', json_encode($data));
+file_put_contents('liste_sellettes.json', json_encode($data));
 
 // Rediriger l'utilisateur vers la page d'accueil
 header('Location: index.html');
